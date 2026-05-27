2ec8b7e4f4bd70a30727b6f29a9656e51e44212b
Haranck <harankrishna03@gmail.com>
2026-05-27 12:18:52 +0530

    feat: initialize server application with CORS configuration and set client environment variable

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,7 +6,7 @@ import urlRoutes from "./routes/urlRoutes";
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://nano-link-lime.vercel.app"],
+  origin: ["http://localhost:5173", "https://n-skz0.onrender.com"],
   credentials: true
 }));
 app.use(express.json());
